d5100b42267c7b7427344eeca4e67a1fa5ef2ccb
Alex Ong <aji@Alexs-MacBook-Pro.local>
2021-04-25 13:55:54 +0800

    Modified parsed data so shows with incomplete air date won't return an error and added div placeholders to modal before data load

--- static/index.js
+++ static/index.js
@@ -1,21 +1,39 @@
-$(function() {
+$(function fill_modal() {
     $(".see-btn").click(function post() {
+        modify_modal();
         var self = $(this);
-        console.log(self.val());
-        console.log(JSON.stringify({handle: self.val()}));
         $.ajax({
             type : "POST",
             url : "/",
             dataType: "json",
             data: JSON.stringify({handle: self.val()}),
             contentType: 'application/json;charset=UTF-8',
-            success: function print(data) {
-                console.log(data);
-                $(dramaInfoModalLabel).html(data.title);
+            success: function(data) {
+                modify_modal(data);
             },
             error:  function(data) {
-                console.log("error");
+                $(dramaInfoModalLabel).html("<span style='color:red'>Sorry!</span>");
+                $(dramaInfoLine1).html("")
+                $(dramaInfoLine2).html("Source data for this drama is inaccessible.")
+                $(dramaInfoLine3).html("");
+                $(dramaInfoLine4).html("");
+                // disable add to calendar button
             }
         });
     });
 });
+
+
+function modify_modal(data="None") {
+    if (data == "None") {
+        $(dramaInfoModalLabel).html("<div id='loading-title'></div>");
+        $(dramaInfoModalBody).children().html("<div class='loading-line mb-4'></div>");
+    }
+    else {
+        $(dramaInfoModalLabel).html(data.title);
+        $(dramaInfoLine1).html("Synopsis: " + data.synopsis);
+        $(dramaInfoLine2).html("Cast: " + data.cast);
+        $(dramaInfoLine3).html("Airing dates: " + data.air_date);
+        $(dramaInfoLine4).html("Schedule: " + data.aired_on);
+    }
+};
