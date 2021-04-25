643ffc143d7e07da032cf28b24d249ca159a033d
Alex Ong <aji@Alexs-MacBook-Pro.local>
2021-04-25 13:55:54 +0800

    Modified parsed data so shows with incomplete air date won't return an error and added div placeholders to modal before data load

--- helper.py
+++ helper.py
@@ -24,38 +24,29 @@ def generate(year, quarter):
     """Look up list of dramas for the given year & quarter"""
 
     # Contact API
-    try:
-        url = f"https://kuryana.vercel.app/seasonal/{year}/{quarter}"
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.RequestException:
-        return None
+    url = f"https://kuryana.vercel.app/seasonal/{year}/{quarter}"
+    response = check_url(url)
 
     # Parse response
     korean_shows = []
-    korean_count = 0
-    temp_count = 0
-    try:
-        results = response.json()
-        for result in results:
-            result_info = {}
-            if result["content_type"] == "Korean Drama":
-                try: 
-                    result_info["title"] = result.setdefault("title", "N/A")
-                    result_info["episodes"] = result.setdefault("episodes", "N/A")
-                    result_info["type"] = result.setdefault("type", "N/A")
-                    result_info["synopsis"] = result.setdefault("synopsis", "N/A")
-                    result_info["released_at"] = result.setdefault("released_at", "N/A")
-                    result_info["url"] = result.setdefault("url", "N/A")
-                    result_info["genres"] = result.setdefault("genres", "N/A")
-                    result_info["thumbnail"] = result.setdefault("thumbnail", "N/A")
-                    korean_shows.append(result_info)
-                    temp_count += 1
-                except:
-                    return None      
-    except (KeyError, TypeError, ValueError):
-        return None
-    
+
+    results = response.json()
+    for result in results:
+        result_info = {}
+        ## Add filter for only airing dramas
+        if result["content_type"] == "Korean Drama":
+            try: 
+                result_info["title"] = result.setdefault("title", "N/A")
+                result_info["episodes"] = result.setdefault("episodes", "N/A")
+                result_info["type"] = result.setdefault("type", "N/A")
+                result_info["synopsis"] = result.setdefault("synopsis", "N/A")
+                result_info["released_at"] = result.setdefault("released_at", "N/A")
+                result_info["url"] = result.setdefault("url", "N/A")
+                result_info["genres"] = result.setdefault("genres", "N/A")
+                result_info["thumbnail"] = result.setdefault("thumbnail", "N/A")
+                korean_shows.append(result_info)
+            except (KeyError, TypeError, ValueError):
+                    return 103      
     if len(korean_shows) != 0:
         return(korean_shows)
 
@@ -66,31 +57,36 @@ def fetch(drama_url):
     # Contact API
     try_counter = 0
     connection_error = True
-    while try_counter < 500 and connection_error == True:
-        try:
-            url = f"https://kuryana.vercel.app/id{drama_url}"
-            response = requests.get(url)
-            response.raise_for_status()
-            connection_error = False
-        except requests.RequestException:
+    url = f"https://kuryana.vercel.app/id{drama_url}"
+    while try_counter < 3 and connection_error == True:
+        response = check_url(url)
+        if response == 102:
             try_counter += 1
-            return None
+        else:
+            connection_error = False
 
     # Parse response
     try:
-        info = response.json()
-        return {
-            "title": info["data"].setdefault("title", "N/A"),
-            "rating": info["data"].setdefault("rating", "N/A"),
-            "poster": info["data"].setdefault("poster", "N/A"),
-            "synopsis": info["data"].setdefault("synopsis", "N/A"),
-            "cast": info["data"].setdefault("casts", "N/A"),
-            "episodes": info["data"]["details"].setdefault("episodes", "N/A"),
-            "start_date": info["data"]["details"].setdefault("aired", "N/A").split("-")[0],
-            "end_date": info["data"]["details"].setdefault("aired", "N/A").split("-")[1],
-            "aired_on": info["data"]["details"].setdefault("aired_on", "N/A"),
-            "network": info["data"]["details"].setdefault("original_network", "N/A")
-        }
+        info_unparsed = response.json()
+        info_parsed = {}
+        info_parsed["title"] = info_unparsed["data"].setdefault("title", "N/A")
+        info_parsed["rating"] = info_unparsed["data"].setdefault("rating", "N/A")
+        info_parsed["poster"] = info_unparsed["data"].setdefault("poster", "N/A")
+        info_parsed["synopsis"] = info_unparsed["data"].setdefault("synopsis", "N/A")
+        info_parsed["cast"] = info_unparsed["data"].setdefault("casts", "N/A")
+        info_parsed["episodes"] = info_unparsed["data"]["details"].setdefault("episodes", "N/A")
+        info_parsed["start_date"] = info_unparsed["data"]["details"].setdefault("aired", "N/A").split("-")[0]
+        info_parsed["air_date"] = info_unparsed["data"]["details"].setdefault("aired", "N/A")
+        info_parsed["aired_on"] = info_unparsed["data"]["details"].setdefault("aired_on", "N/A")
+        info_parsed["network"] = info_unparsed["data"]["details"].setdefault("original_network", "N/A")
+        return info_parsed
     except (KeyError, TypeError, ValueError):
-        print("error")
-        return None
+        return 104
+
+def check_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response
+    except requests.RequestException:
+        return 102
