050a00c0d7950eae55136f54ad95411c3d521bca
Alex Ong <aji@Alexs-MacBook-Pro.local>
2021-05-25 17:14:47 +0800

    Removed no caching settings from app.py

--- app.py
+++ app.py
@@ -17,12 +17,12 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+# @app.after_request
+# def after_request(response):
+    # response.headers["Cache-Control"] = "no-cache, must-revalidate"
+    # response.headers["Expires"] = 0
+    # response.headers["Pragma"] = "no-cache"
+    # return response
 
 @app.route("/kalendar", methods=["GET"])
 def calendar():
